c72a625ed757d45334aaa9cf250793068595cff6
vivekanandd1 <vivekanandd1@live.com>
2023-08-18 15:20:29 +0530

    initial file

---
+++ src/test/java/Session/BrowserLaunch.java
@@ -0,0 +1,14 @@
+package Session;
+
+import com.microsoft.playwright.Playwright;
+
+public class BrowserLaunch {
+	
+	public static void main(String[] args) {
+		
+		Playwright playwright = Playwright.create();
+		
+	}
+	
+
+}
