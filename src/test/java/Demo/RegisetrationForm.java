351883df7d7a810b6dc63dcbe81fcc358a6e6804
vivekanandd1 <vivekanandd1@live.com>
2023-08-24 00:04:26 +0530

    All code

--- src/test/java/Demo/RegisetrationForm.java
+++ src/test/java/Demo/RegisetrationForm.java
@@ -6,7 +6,7 @@ import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 
 public class RegisetrationForm {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Playwright playwright = Playwright.create();
 		Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
 		Page page = browser.newPage();
@@ -15,8 +15,12 @@ public class RegisetrationForm {
 		page.locator("input[name='lastname']").fill("shalby");
 		page.locator("//input[@type='email']").fill("shalby.john@gmail.com");
 		page.locator("//input[@type='password']").fill("john@123");
+	    Thread.sleep(3000);
+		page.locator("#input-newsletter-yes").click();
+		page.locator(".form-check-input").last().click();
+		Thread.sleep(3000);
 		page.locator("text=Continue").click();
-		
+		Thread.sleep(3000);
 		browser.close();
 		
 		playwright.close();
